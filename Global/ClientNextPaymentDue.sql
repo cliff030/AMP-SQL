f2bdc427f2fcca9f75d695af5bd017bdace1fcc1
chris-brundage <christopher.m.brundage@gmail.com>
2015-07-16 22:45:51 -0500

    Ticket #1065. Corrected function that returns the client's next payment due date to only look at payments for CreditorID 0.

--- Global/ClientNextPaymentDue.sql
+++ Global/ClientNextPaymentDue.sql
@@ -1,4 +1,10 @@
-ALTER FUNCTION dbo.Custom_ClientNextPaymentDue (   
+IF OBJECT_ID('dbo.Custom_ClientNextPaymentDue') IS NOT NULL
+BEGIN
+	DROP FUNCTION dbo.Custom_ClientNextPaymentDue
+END
+GO
+
+CREATE FUNCTION dbo.Custom_ClientNextPaymentDue (   
 	@ClientID int  
 ) RETURNS datetime 
 BEGIN   
@@ -9,10 +15,14 @@ SET @DueDate=(
 				FROM ProgramScheduleDebtor AS psd
 				INNER JOIN ProgramPayments AS ps
 					ON ps.ProgramScheduleDebtorID = psd.ProgramScheduleDebtorID
+				INNER JOIN ClientCred AS cc
+					ON ps.ClientCredID = cc.ClientCredID
 				WHERE ps.ClientID = @ClientID
 				AND dbo.StartOfDay(psd.DueDate) > dbo.StartOfDay(GETDATE())
+				AND cc.CreditorID = 0
 				ORDER BY psd.DueDate ASC
 )   
 RETURN @DueDate
 
 END
+GO
